65bfbc3a5bd366b9826493e76674efb3a8c3a5d7
K0infig <71273189+K0infig@users.noreply.github.com>
2021-08-31 06:22:28 +0530

    Add files via upload

---
+++ ComputerBase.js
@@ -0,0 +1,24 @@
+class ComputerBase{
+    constructor(x, y, width, height){
+    
+    var options = {
+      isStatic:true
+    };
+    
+    this.body = Bodies.rectangle(x, y, width, height, options);
+    this.w = width;
+    this.h = height;
+    World.add(world, this.body);
+   
+    }
+    display(){
+    let pos = this.body.position; 
+
+
+     push();
+      rectMode(CENTER);
+      fill("black");
+      rect(pos.x, pos.y, this.w, this.h);
+     pop();
+    }
+}
